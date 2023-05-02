008e22afb8e382630f17a54b81163aa7ad16739c
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-05-02 19:57:57 +0800

    set up chakraui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,20 @@ import { type AppType } from "next/app";
 
 import { api } from "@/utils/api";
 
+import { ChakraProvider } from "@chakra-ui/react";
 import "@/styles/globals.css";
 
+const AppWrapper = ({ children }: { children: React.ReactNode }) => {
+  // const { mode } = useGlobalContext();
+  return <ChakraProvider>{children}</ChakraProvider>;
+};
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <AppWrapper>
+      <Component {...pageProps} />
+    </AppWrapper>
+  );
 };
 
 export default api.withTRPC(MyApp);

25ca80ffc402972debf71ff81d5a1835b1bf7633
Timothy Chia <timothycth9@gmail.com>
2023-05-05 11:06:41 +0800

    so sorry for bad ui sithu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { type AppType } from "next/app";
+import { ChakraProvider } from '@chakra-ui/react'
+import { AuthProvider } from "@/context";
+
 
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+    <ChakraProvider>
+    <Component {...pageProps} />
+  </ChakraProvider>
+  </AuthProvider>
+  )
 };
 
 export default api.withTRPC(MyApp);

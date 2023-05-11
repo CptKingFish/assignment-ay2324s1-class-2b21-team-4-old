87b37cfa07873e23d5fd54d0e651412241f66c45
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-05-11 11:44:00 +0800

    name changed to SideBarNav

---
+++ src/components/SideBarNav.tsx
@@ -0,0 +1,143 @@
+import ChatList from "./ChatList";
+import ChatMenuItem from "./ChatMenuItem";
+import React from "react";
+
+const chatInfoArr = [
+  {
+    id: "1",
+    avatarUrl: "https://i.pravatar.cc/300?img=1",
+    name: "John Doe",
+    lastMessage: "Hello, World!",
+    lastMessageTime: "9:45 PM",
+  },
+  {
+    id: "2",
+    avatarUrl: "https://i.pravatar.cc/300?img=1",
+    name: "John Doe",
+    lastMessage: "Hello, World!",
+    lastMessageTime: "9:45 PM",
+  },
+];
+
+export default function SideBarNav({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [activeTab, setActiveTab] = React.useState(0);
+  const handleTabChange = (index: number) => {
+    setActiveTab(index);
+  };
+  return (
+    <div className="drawer-mobile drawer">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        <div>{children}</div>
+        <label
+          htmlFor="my-drawer-2"
+          className="btn-primary drawer-button btn lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="base-200 menu relative w-80 bg-secondary p-4">
+          <div>sidebar content</div>
+          <div className="tabs mb-4 w-full">
+            <a
+              className={
+                "tab-bordered tab w-1/3 " +
+                (activeTab === 0 ? " tab-active" : "")
+              }
+              onClick={() => handleTabChange(0)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+                />
+              </svg>
+            </a>
+            <a
+              className={
+                "tab-bordered tab w-1/3 " +
+                (activeTab === 1 ? " tab-active" : "")
+              }
+              onClick={() => handleTabChange(1)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+                />
+              </svg>
+            </a>
+            <a
+              className={
+                "tab-bordered tab w-1/3 " +
+                (activeTab === 2 ? " tab-active" : "")
+              }
+              onClick={() => handleTabChange(2)}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
+                />
+              </svg>
+            </a>
+          </div>
+          <input
+            type="text"
+            placeholder="Search chat"
+            className="input w-full max-w-xs"
+          />
+          <div>
+            <ChatList chatInfoArr={chatInfoArr} />
+          </div>
+          <button className="btn-outline glass btn-circle btn absolute bottom-10 right-10">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,139 +1,70 @@
-import ChatList from "./ChatList";
-import ChatMenuItem from "./ChatMenuItem";
-import React from "react";
+import React, { useState } from 'react';
 
-const chatInfoArr = [
-  {
-    id: "1",
-    avatarUrl: "https://i.pravatar.cc/300?img=1",
-    name: "John Doe",
-    lastMessage: "Hello, World!",
-    lastMessageTime: "9:45 PM",
-  },
-  {
-    id: "2",
-    avatarUrl: "https://i.pravatar.cc/300?img=1",
-    name: "John Doe",
-    lastMessage: "Hello, World!",
-    lastMessageTime: "9:45 PM",
-  },
-];
 
-export default function Sidebar({ children }: { children: React.ReactNode }) {
-  const [activeTab, setActiveTab] = React.useState(0);
-  const handleTabChange = (index: number) => {
-    setActiveTab(index);
-  };
-  return (
-    <div className="drawer-mobile drawer">
-      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col items-center justify-center">
-        <div>{children}</div>
-        <label
-          htmlFor="my-drawer-2"
-          className="btn-primary drawer-button btn lg:hidden"
-        >
-          Open drawer
-        </label>
-      </div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-        <ul className="base-200 menu relative w-80 bg-secondary p-4">
-          <div>sidebar content</div>
-          <div className="tabs mb-4 w-full">
-            <a
-              className={
-                "tab-bordered tab w-1/3 " +
-                (activeTab === 0 ? " tab-active" : "")
-              }
-              onClick={() => handleTabChange(0)}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
-                />
-              </svg>
-            </a>
-            <a
-              className={
-                "tab-bordered tab w-1/3 " +
-                (activeTab === 1 ? " tab-active" : "")
-              }
-              onClick={() => handleTabChange(1)}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
-                />
-              </svg>
-            </a>
-            <a
-              className={
-                "tab-bordered tab w-1/3 " +
-                (activeTab === 2 ? " tab-active" : "")
-              }
-              onClick={() => handleTabChange(2)}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-6 w-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
-                />
-              </svg>
-            </a>
-          </div>
-          <input
-            type="text"
-            placeholder="Search chat"
-            className="input w-full max-w-xs"
-          />
-          <div>
-            <ChatList chatInfoArr={chatInfoArr} />
-          </div>
-          <button className="btn-outline glass btn-circle btn absolute bottom-10 right-10">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-          </button>
-        </ul>
-      </div>
-    </div>
-  );
-}
+const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(true);
+
+    function handleButtonClick() {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <div className="drawer drawer-mobile drawer-end ">
+            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+                <div className="navbar bg-base-100">
+                    <div className="flex-1">
+                        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    </div>
+                    <div className="flex-none">
+                        <ul className="menu menu-horizontal px-1">
+
+                            <li><button className={"btn btn-outline btn-info " + (isOpen ? "hidden" : "")} onClick={handleButtonClick}>Open Menu</button></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div className="drawer-side">
+                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                <ul className={"items-center content-center border border-white menu p-4 w-80 bg-base-100 text-base-content " + (isOpen ? "" : "hidden")}>
+                    <button className="btn btn-outline btn-info" onClick={handleButtonClick}>Close Menu</button>
+                    <div className="divider"></div>
+
+                    <div className="avatar mb-5 btn">
+                        <div className="w-24 rounded-full">
+                            <button className = "">
+                                <img src="https://picsum.photos/200/300" />
+                            </button>
+                        </div>
+                    </div>
+                    <br/>
+                    <div className="mt-5 badge ">
+                        Group Name
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                            </svg>
+                        </button>
+
+                    </div>
+                    <div className="divider "></div>
+                    <div className='mt-5 badge'>SCRUM</div>
+                    <div className='mt-5 badge'>SCRUM</div>
+                    <div className='mt-5 badge'>SCRUM</div>
+                    <div className='mt-5 badge'>SCRUM</div>
+                    <div className='mt-5 badge'>IDK</div>
+                    <div className="divider "></div>
+                    <div className="mt-5 badge ">
+                        Participants
+                    </div>
+                    <div className="divider "></div>
+                    
+                </ul>
+
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;

5bf809627ea4ad90a11eba24e8464715da39987b
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-05-16 19:31:47 +0800

    f

---
+++ src/pages/privatechat/[id].tsx
@@ -0,0 +1,151 @@
+import React, { useEffect } from "react";
+import { useGlobalContext } from "@/context";
+import { pusherClientConstructor } from "@/utils/pusherConfig";
+import TopNav from "@/components/TopNav";
+import ChatInput from "@/components/ChatInput";
+import { useRouter } from "next/router";
+import ChatBody from "@/components/ChatBody";
+import { Message } from "@/utils/chat";
+import { api } from "@/utils/api";
+import UserSideBar from "@/components/UserSideBar";
+
+const TeamChat = () => {
+  const router = useRouter();
+  const { user } = useGlobalContext();
+  const [messages, setMessages] = React.useState<Message[]>([]);
+  const [users, setUsers] = React.useState<Object[]>([]);
+  // const [showDownButton, setShowDownButton] = React.useState(false);
+  const {
+    data: chatroomData,
+    refetch,
+    isLoading,
+  } = api.chat.getMessagesAndChatroomInfo.useQuery({
+    chatroom_id: router.query.id as string,
+  });
+
+  const { data: userRaw } = api.chat.getUsernamesFromChatroom.useQuery({
+    chatroom_id: router.query.id as string,
+  });
+
+  const [isOpen, setIsOpen] = React.useState(true);
+
+  function handleDrawerToggle() {
+    setIsOpen(!isOpen);
+  }
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     console.log("window.pageYOffset", window.pageYOffset);
+
+  //     if (window.pageYOffset > 100) {
+  //       setShowDownButton(true);
+  //     } else {
+  //       setShowDownButton(false);
+  //     }
+  //   };
+
+  //   window.addEventListener("scroll", handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
+
+  // const handleDownButtonClick = () => {
+  //   window.scrollTo({
+  //     top: document.body.scrollHeight,
+  //     behavior: "smooth",
+  //   });
+  // };
+
+  useEffect(() => {
+    if (isLoading || !chatroomData) return;
+    setMessages(chatroomData.messages);
+    setUsers(
+      (userRaw || []).map((user) => {
+        return { key: user._id, username: user.username };
+      })
+    );
+  }, [isLoading, chatroomData, userRaw]);
+
+  const channelCode = React.useMemo(() => {
+    return "presence-" + (router.query.id as string);
+  }, [router.query.id]);
+
+  React.useEffect(() => {
+    if (!user) return;
+
+    const pusherClient = pusherClientConstructor(user?._id);
+    console.log("pusherClient", pusherClient);
+
+    pusherClient.subscribe(channelCode);
+
+    const messageHandler = (message: Message) => {
+      console.log("incoming message", message);
+
+      setMessages((prev) => [message, ...prev]);
+    };
+
+    pusherClient.bind("incoming-message", messageHandler);
+
+    return () => {
+      pusherClient.unsubscribe(channelCode);
+      pusherClient.unbind("incoming-message", messageHandler);
+    };
+  }, [user, channelCode]);
+
+  // const scrollDownRef = React.useRef<HTMLDivElement | null>(null);
+
+  // const scrollToBottom = () => {
+  //   scrollDownRef.current?.scrollIntoView({ behavior: "smooth" });
+  // };
+
+  return (
+    <>
+      <TopNav
+        chatroom_name={chatroomData?.name || ""}
+        openSidebarDetails={handleDrawerToggle}
+      />
+      <div className="drawer drawer-mobile drawer-end">
+        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+
+        <div className="flex-no-wrap drawer-content flex">
+          {/* {showDownButton && (
+            <div className="fixed bottom-10 right-4 z-50">
+              <button
+                onClick={handleDownButtonClick}
+                className="rounded-full bg-blue-500 px-4 py-2 font-bold text-white shadow hover:bg-blue-700"
+              >
+                Scroll down
+              </button>
+            </div>
+          )} */}
+
+          <div className="relative flex h-full max-h-[calc(100vh-6rem)] flex-1 flex-col">
+            <div className="scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch flex h-full flex-1 flex-col-reverse gap-4 overflow-y-auto p-3 pb-16">
+              <ChatBody messages={messages} />
+            </div>
+            <ChatInput channelCode={channelCode} />
+          </div>
+
+          {/*
+         <button
+            className={` h-screen items-center justify-center bg-base-200 px-2 text-4xl text-white ${
+              isOpen ? "hidden" : ""
+            }`}
+            onClick={handleDrawerToggle}
+          >
+            {"<"}
+          </button> */}
+        </div>
+        <UserSideBar
+          isOpen={isOpen}
+          handleDrawerToggle={handleDrawerToggle}
+          participants={users}
+        />
+      </div>
+    </>
+  );
+};
+
+export default TeamChat;

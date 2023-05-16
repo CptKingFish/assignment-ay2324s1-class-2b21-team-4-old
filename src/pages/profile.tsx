c05edbc3f8319c2dc3782023e9ef5721f802c6ec
TimelesClock <leongandy027@gmail.com>
2023-05-16 18:46:03 +0800

    initial

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,13 +1,16 @@
 import Profile from "@/components/Profile";
 import ProfileSideBar from "@/components/ProfileSideBar";
 import React from "react";
+import Avatar from "react-avatar-edit"
 
 const profile = () => {
     return (
-        <div className="drawer drawer-mobile">
-            <Profile/>
-            <ProfileSideBar />
-        </div>
+        <>
+            <div className="drawer drawer-mobile">
+                <Profile />
+                <ProfileSideBar />
+            </div>
+        </>
     )
 };
 

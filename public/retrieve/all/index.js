b2184663fbf0221a20c25bf7afaf42a724a4f563
Kaung Sithu Hlaing <kaungsithuhlaing@gmail.com>
2023-05-20 13:27:01 +0800

    deleted bed recap files

--- public/retrieve/all/index.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('DOMContentLoaded', function () {
-    // TODO: Fetch all modules in the database from the Backend
-    // TODO: Add each row returned onto the HTML table using the addToTable function found in ../helper.js
-});
--- server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./app');
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-    // eslint-disable-next-line no-console
-    console.log(`App listening on port ${port}`);
-});

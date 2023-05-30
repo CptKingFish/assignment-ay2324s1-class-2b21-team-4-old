10be5f4eee5c811c740f13c6e7603ae71afc9bab
Timothy Chia <timothycth9@gmail.com>
2023-05-30 22:45:21 +0800

    all works now

--- src/html/register.ts
+++ src/html/register.ts
@@ -182,7 +182,7 @@ export function registerHtml(token: string) {
                       <table border="0" cellpadding="0" cellspacing="0">
                         <tr>
                           <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                            <a href="${env.BASE_URL}/verify-email?token=${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
+                            <a href="${env.BASE_URL}/verify-email/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Verify Email</a>
                           </td>
                         </tr>
                       </table>
